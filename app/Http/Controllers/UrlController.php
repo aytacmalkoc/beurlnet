727b6190b207d227ef16f92c18d7d06ee67ee320
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-09-08 13:40:57 +0300

    REMOVE unused controller methods

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -17,72 +17,7 @@ class UrlController extends Controller
      */
     public function index()
     {
-        return view('homepage', ['public_links' => Url::where('public', true)->limit(12)->get() ?? []]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request)
-    {
-        $client = new Client();
-        $short_url = $client->generateId(8);
-
-        return $this->store((object)[
-            'url' => $request->url,
-            'short_url' => $short_url,
-            'public' => $request->public || false
-        ]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  $data
-     * @return \Illuminate\Http\Response
-     */
-    public function store($data)
-    {
-        Url::insert([
-            'url' => $data->url,
-            'short_url' => $data->short_url,
-            'public' => $data->public,
-            'created_at' => now()
-        ]);
-
-        \url_counter('created');
-
-        Log::channel('url')->info("Yeni bir url eklendi. [https://beurl.net/{$data->short_url}]");
-
-        return response()->json("beurl.net/{$data->short_url}");
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  $url
-     * @return \Illuminate\Http\Response
-     */
-    public function show($short_url)
-    {
-        $url = Url::where('short_url', $short_url)->first();
-
-        if ($url) {
-            \url_counter('visited');
-            
-            Log::channel('url')->info("Url yönlendirildi. [https://beurl.net/{$short_url} => {$url->url}]");
-    
-            if (!array_key_exists('scheme', parse_url($url->url))) {
-                return redirect("https://{$url->url}");
-            }else {
-                return redirect($url->url);
-            }
-        }
-
-        return abort(404, 'Bağlantı bulunamadı. Bu bağlantı silinmiş olabilir.');
-
+        $urls = Url::where('visible', true)->get();
+        return view('web.pages.Home', compact('urls'));
     }
 }
