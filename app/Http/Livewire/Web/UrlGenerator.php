aa649d89b7cfe8f3ba3e64193e43e9b690514d0c
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-09-08 21:43:56 +0300

    Create store methods & properties

--- app/Http/Livewire/Web/UrlGenerator.php
+++ app/Http/Livewire/Web/UrlGenerator.php
@@ -2,10 +2,49 @@
 
 namespace App\Http\Livewire\Web;
 
+use App\Models\Url;
+use Hidehalo\Nanoid\Client;
 use Livewire\Component;
 
 class UrlGenerator extends Component
 {
+    public string $url = '';
+    public bool $visible = false;
+    public string $result = '';
+
+    public function updated($field)
+    {
+        $this->validateOnly($field, [
+            'url' => 'required|url',
+            'visible' => 'boolean'
+        ]);
+    }
+
+    public function createUrl()
+    {
+        $this->validate();
+        $this->result = '';
+
+        $client = new Client();
+        $shortened = $client->generateId(8);
+
+        $url = Url::create([
+            'original' => $this->url,
+            'shortened' => $shortened,
+            'full_url' => \url($shortened),
+            'visible' => $this->visible
+        ]);
+
+        $this->result = $url->withoutHttps('full_url');
+        $this->clear();
+    }
+
+    private function clear()
+    {
+        $this->url = '';
+        $this->visible = false;
+    }
+
     public function render()
     {
         return view('web.components.url-generator');
--- resources/views/web/components/url-generator.blade.php
+++ resources/views/web/components/url-generator.blade.php
@@ -1,15 +1,20 @@
 <div>
-    <form class="rounded p-4 mt-4 bg-white">
+    <form wire:submit.prevent="createUrl" class="rounded p-4 mt-4 bg-white">
         <div class="row text-left justify-content-center">
             <div class="col-lg-7 col-md-12">
                 <div class="form-group mb-3">
-                    <input type="url" class="form-control" placeholder="url"/>
+                    <input type="url" class="form-control" placeholder="url" wire:model.lazy="url" required/>
+                    @error('url')
+                    <div class="invalid-feedback d-block">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
             </div>
 
             <div class="col-lg-3 col-md-12">
                 <div class="form-group mb-3">
-                    <select class="form-control custom-select">
+                    <select class="form-control custom-select" wire:model="visible">
                         <option value="0">Gizle</option>
                         <option value="1">Göster</option>
                     </select>
@@ -21,6 +26,18 @@
                     Kısalt!
                 </button>
             </div>
+
+            @if($result)
+                <div class="col-12">
+                    <div class="alert alert-light text-center">
+                        <p class="text-muted mb-0 mt-4 small">
+                            <a href="https://{{ $result }}" target="_blank" class="text-success">{{ $result }}</a>
+                            <button class="btn btn-outline-light mx-3 btn-sm">kopyala</button>
+                        </p>
+                    </div>
+                </div>
+            @endif
+
         </div>
     </form>
 </div>
