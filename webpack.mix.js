e7ddd49f92175ee2c523c4b1b0d33e711bdc5ddf
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-09-08 13:10:56 +0300

    REMOVE VueJS configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,5 @@
 const mix = require("laravel-mix");
 
-mix.js("resources/js/app.js", "public/assets/js")
-    .vue()
-    .sass("resources/sass/app.scss", "public/assets/css/app.min.css")
-    .options({
-        processCssUrls: false,
-    });
+mix.sass("resources/sass/app.scss", "public/assets/css/app.min.css").options({
+    processCssUrls: false,
+});
