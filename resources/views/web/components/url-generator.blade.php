007032f547fa2389f7477f7e6019e2adbfb7f857
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-09-08 20:37:49 +0300

    Create livewire components

---
+++ resources/views/web/components/url-generator.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <form class="rounded p-4 mt-4 bg-white">
+        <div class="row text-left justify-content-center">
+            <div class="col-lg-7 col-md-12">
+                <div class="form-group mb-3">
+                    <input type="url" class="form-control" placeholder="url"/>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-12">
+                <div class="form-group mb-3">
+                    <select class="form-control custom-select">
+                        <option value="0">Gizle</option>
+                        <option value="1">Göster</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-12">
+                <button type="submit" class="searchbtn btn btn-outline-success btn-block p">
+                    Kısalt!
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
