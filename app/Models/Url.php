938a1d15102edb962dc8235046313c5aadd4b40d
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-09-08 20:38:37 +0300

    Add methods to Url model

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Url extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function withoutHttps($attribute)
+    {
+        return preg_replace('#^https?://#', '', rtrim($this->getAttribute($attribute), '/'));
+    }
+
+    public function domain()
+    {
+        return parse_url($this->getAttribute('original'), PHP_URL_HOST);
+    }
 }
